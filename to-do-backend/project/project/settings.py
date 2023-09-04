7c32f9f3a910cec4c51c64a61dfb8ee159c4747c
OverkillViper <117332017+OverkillViper@users.noreply.github.com>
2023-09-05 00:59:37 +0600

    Collected all static files

--- to-do-backend/project/project/settings.py
+++ to-do-backend/project/project/settings.py
@@ -151,10 +151,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
